576644267b94d8c488e418740040dd878ae287f8
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-08-30 09:48:51 -0400

    MERN-Stack Infrastructure

---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,15 @@
+export default function NewOrderPage() {
+  return (
+      <h1>New Order Page</h1>
+  )
+}
+
+// import React from 'react'
+
+// export default function NewOrderPage() {
+//   return (
+//     <div>
+//       <h1> New Order page</h1>
+//     </div>
+//   )
+// }
